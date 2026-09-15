b95318063b2c1fce8511e6d78856b842f56f4083
evgesha <orele0961@gmail.com>
2026-09-15 18:00:11 +0200

    Simplify 08-09 homework

--- Homework/08-09.kt
+++ Homework/08-09.kt
@@ -4,178 +4,159 @@ package lessons.lesson03.homework
 val eventName: String = "Hackathon Survival"
 
 // 2. Дата проведения
-var eventDate: String = "20 сентября 2026"
-    private set
+var eventDate: String = "8 сентября"
 
 // 3. Место проведения
-var venue: String = "Технопарк"
-    private set
+var eventPlace: String = "IT школа"
 
-// 4. Подробный бюджет мероприятия, включая расходы на оборудование, кейтеринг и другие операционные расходы
-private val detailedBudget: String = "Оборудование, кейтеринг и другие операционные расходы"
+// 4. Подробный бюджет мероприятия
+private val detailedBudget: String = "Еда, техника, аренда"
 
 // 5. Количество участников
-var participantCount: Int = 0
-    private set(value) {
-        require(value >= 0) { "Количество участников не может быть отрицательным" }
-        field = value
-    }
+var participantsCount: Int = 100
+    private set
 
 // 6. Длительность хакатона
-val hackathonDuration: String = "48 часов"
+val duration: String = "2 дня"
 
-// 7. Контактная информация и условия соглашений с поставщиками пищи, оборудования и других услуг
-private val supplierAgreements: String = "Контакты поставщиков и условия договоров"
+// 7. Контакты и условия соглашений с поставщиками
+private var suppliersInfo: String = "Контакты поставщиков"
 
-// 8. Текущее состояние хакатона (статус)
-var hackathonStatus: String = "Подготовка"
-    private set
+// 8. Текущее состояние хакатона
+var status: String = "Подготовка"
 
 // 9. Список спонсоров
-val sponsors: String = "Список спонсоров уточняется"
+val sponsors: String = "Список спонсоров"
 
 // 10. Бюджет мероприятия
-val eventBudget: Int = 1_000_000
+val budget: Int = 500000
 
 // 11. Текущий уровень доступа к интернету
-var internetAccessLevel: String = "Полный"
-    private set
+var internetAccess: String = "Хороший"
 
-// 12. Информация о транспортировке оборудования, распределении ресурсов и координации команд поддержки
-private var logisticsInformation: String = "Внутренний план логистики и распределения ресурсов"
+// 12. Информация о перевозке оборудования и распределении ресурсов
+private var equipmentDeliveryInfo: String = "План перевозки техники"
 
 // 13. Количество команд
-var teamCount: Int = 0
-    private set(value) {
-        require(value >= 0) { "Количество команд не может быть отрицательным" }
-        field = value
+var teamsCount: Int = 10
+    set(value) {
+        if (value >= 0) field = value
     }
 
 // 14. Перечень задач
-val taskList: String = "Список задач хакатона"
+val tasks: String = "Список задач"
 
 // 15. План эвакуации
-val evacuationPlan: String = "План и схема эвакуации"
+val evacuationPlan: String = "План эвакуации находится у выхода"
 
-// 16. Список доступного оборудования (всё, что выделено для использования на мероприятии)
-val allocatedEquipment: String = "Ноутбуки, мониторы, роутеры и наборы электроники"
+// 16. Список доступного оборудования
+val allEquipment: String = "Ноутбуки, мониторы, удлинители"
 
-// 17. Список свободного оборудования (всё, что можно взять прямо сейчас и что не занято другими участниками)
-val freeEquipment: String
-    get() = "Актуальный список незанятого оборудования"
-
-// 18. График питания участников (зависит от поставщика питания, определяемого за неделю до начала)
-lateinit var mealSchedule: String
+// 17. Список свободного оборудования
+var freeEquipment: String = "Свободные ноутбуки и мониторы"
     private set
 
-// 19. План мероприятий на случай сбоев
-var failureResponsePlan: String = "Резервное питание, запасное оборудование и локальная сеть"
-    private set
+// 18. График питания участников, который станет известен за неделю до начала
+lateinit var foodSchedule: String
+
+// 19. План действий на случай сбоев
+var failurePlan: String = "Использовать запасную технику"
 
 // 20. Список экспертов и жюри
-lateinit var expertsAndJury: String
-    private set
+lateinit var juryList: String
 
 // 21. Политика конфиденциальности
-val privacyPolicy: String = "Правила сбора, хранения и обработки данных участников"
+val privacyPolicy: String = "Политика конфиденциальности"
 
-// 22. Приватные отзывы участников и зрителей для анализа проблем
-private var privateFeedback: String = "Отзывы ещё не собраны"
+// 22. Приватные отзывы участников и зрителей
+private var feedback: String = "Отзывов пока нет"
 
 // 23. Текущая температура в помещении
 var roomTemperature: Int = 22
-    private set(value) {
-        require(value in -50..60) { "Недопустимое значение температуры" }
-        field = value
+    set(value) {
+        if (value in -20..50) field = value
     }
 
-// 24. Данные мониторинга и анализ производительности сети для отладки приложений командами
-val networkMonitoringData: String
-    get() = "Актуальные показатели задержки, пропускной способности и потерь пакетов"
+// 24. Данные мониторинга работы сети для команд
+val networkInfo: String
+    get() = "Текущие данные сети"
 
-// 25. Регулятор скорости интернета (коэффициент хранится целым процентом: 50 означает 0.5)
-private var internetSpeedPercent: Int = 100
+// 25. Регулятор скорости интернета в процентах, где 50 процентов равно коэффициенту 0.5
+private var internetSpeed: Int = 100
     set(value) {
-        require(value in 0..100) { "Коэффициент должен быть от 0 до 100 процентов" }
-        field = value
+        if (value in 0..100) field = value
     }
 
 // 26. Уровень освещения
-var lightingLevel: Int = 70
-    private set(value) {
-        require(value in 0..100) { "Уровень освещения должен быть от 0 до 100" }
-        field = value
-    }
+var lightLevel: Int = 70
 
 // 27. Лог событий мероприятия
-private var eventLog: String = "Лог создан"
+private var eventLog: String = "Начало лога"
 
-// 28. Возможность получения медицинской помощи прямо на мероприятии
-val medicalHelpAvailable: Boolean = true
+// 28. Можно ли получить медицинскую помощь на мероприятии
+val medicalHelp: Boolean = true
 
-// 29. Планы и процедуры безопасности, включая эвакуацию и протоколы чрезвычайных ситуаций
-private val securityProcedures: String = "Внутренние инструкции службы безопасности"
+// 29. Планы и процедуры безопасности мероприятия
+private val safetyPlan: String = "План безопасности"
 
 // 30. Регистрационный номер мероприятия
-val registrationNumber: String = "HS-2026-001"
+val registrationNumber: String = "HACK-08-09"
 
-// 31. Максимально допустимый уровень шума в помещении хакатона
-val maximumNoiseLevel: Int = 85
+// 31. Максимально допустимый уровень шума
+val maxNoiseLevel: Int = 80
 
-// 32. Индикатор превышения уровня шума в помещениях
-var noiseLimitExceeded: Boolean = false
+// 32. Превышен ли уровень шума сейчас
+var noiseIsTooHigh: Boolean = false
     private set
 
 // 33. План взаимодействия с прессой
-private var pressInteractionPlan: String = "Интервью проводятся через пресс-службу"
+private var pressPlan: String = "Общение через организатора"
 
-// 34. Детальная публичная информация о проектах команд, требующая компиляции и сбора статистики проверок
-val teamProjectsInformation: String by lazy {
-    "Скомпилированные проекты команд и статистика автоматизированных проверок"
+// 34. Информация о проектах команд, для которой нужны компиляция и проверки
+val projectsInfo: String by lazy {
+    "Информация о проектах и результатах тестов"
 }
 
-// 35. Статус получения всех необходимых разрешений для проведения мероприятия
-var permitsReceived: Boolean = false
+// 35. Получены ли все разрешения на проведение мероприятия
+var allPermissionsReceived: Boolean = false
     private set
 
-// 36. Индикатор доступности специализированного оборудования в данный момент
-var specializedEquipmentAvailable: Boolean = false
+// 36. Доступно ли специальное оборудование сейчас
+var specialEquipmentAvailable: Boolean = true
     private set
 
-// 37. Список партнёров мероприятия
-val eventPartners: String = "Список партнёров мероприятия"
+// 37. Список партнеров мероприятия
+val partners: String = "Список партнеров"
 
-// 38. Итоговый отчёт с фотографиями, видео и отзывами, доступный после завершения мероприятия
+// 38. Отчет с фото, видео и отзывами после окончания мероприятия
 val finalReport: String by lazy {
-    "Итоговый отчёт с фотографиями, видео и отзывами"
+    "Отчет после хакатона"
 }
 
 // 39. Внутренние правила распределения призов
-private val prizeDistributionRules: String = "Внутренние правила определения победителей"
+private val prizeRules: String = "Правила для организаторов"
 
 // 40. Список разыгрываемых призов
-val prizes: String = "Ноутбуки, сертификаты и подписки на сервисы"
+val prizes: String = "Ноутбук, наушники, сертификаты"
 
-// 41. Контакты экстренных служб, медицинского персонала и других важных служб для внутреннего пользования
-private val emergencyContacts: String = "Контакты экстренных служб и ответственного персонала"
+// 41. Контакты экстренных служб и медицинского персонала
+private val emergencyContacts: String = "Телефоны важных служб"
 
 // 42. Особые условия для участников с ограниченными возможностями
-val accessibilityConditions: String = "Безбарьерный вход, лифт и доступное рабочее место"
+val specialConditions: String = "Пандус и удобные рабочие места"
 
-// 43. Общее настроение участников (определяется опросами)
-var participantMood: String = "Опрос ещё не проведён"
+// 43. Общее настроение участников по результатам опросов
+var participantsMood: String = "Хорошее"
     private set
 
-// 44. Подробный план хакатона, инициализируемый непосредственно перед началом мероприятия
-lateinit var detailedHackathonPlan: String
-    private set
+// 44. Подробный план хакатона, который будет известен перед началом
+lateinit var hackathonPlan: String
 
-// 45. Имя знаменитого специального гостя, которое объявят за день до мероприятия
+// 45. Имя специального гостя, которое объявят за день до мероприятия
 lateinit var specialGuestName: String
-    private set
 
-// 46. Максимальное количество людей, которое может вместить место проведения
-val venueCapacity: Int = 500
+// 46. Максимальное количество людей в месте проведения
+val maximumPeople: Int = 200
 
-// 47. Количество часов, отведённое каждой команде для работы над проектом
-val hoursPerTeam: Int = 48
+// 47. Количество часов на работу над проектом для каждой команды
+val workHours: Int = 48
